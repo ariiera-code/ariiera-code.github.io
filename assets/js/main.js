6fc3057ae3f31aa9aa10b50d9b896e141d611e8b
ariiera-code <nibrosmaverick3@gmail.com>
2021-07-27 20:09:57 +0700

    Updated Shoppingskuy backend experience

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,3 +121,39 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+$(document).ready(function () {
+		$("button").click(function (event) {
+				event.preventDefault();
+				CopyToClipboard("thankyounibros@gmail.com", true, "Email copied to clipboard");
+		});
+});
+
+function CopyToClipboard(value, showNotification, notificationText) {
+		var $temp = $("<input>");
+		$("body").append($temp);
+		$temp.val(value).select();
+		document.execCommand("copy");
+		$temp.remove();
+
+		if (typeof showNotification === 'undefined') {
+				showNotification = true;
+		}
+		if (typeof notificationText === 'undefined') {
+				notificationText = "Copied to clipboard";
+		}
+
+		var notificationTag = $("div.copy-notification");
+		if (showNotification && notificationTag.length == 0) {
+				notificationTag = $("<div/>", { "class": "copy-notification", text: notificationText });
+				$("body").append(notificationTag);
+
+				notificationTag.fadeIn("slow", function () {
+						setTimeout(function () {
+								notificationTag.fadeOut("slow", function () {
+										notificationTag.remove();
+								});
+						}, 1000);
+				});
+		}
+}
